74e85f26b7ebf47e8cad01fa5bc01446a3d9b6a6
Vaishnavi  Nikam <150416104+VaishDataGeek@users.noreply.github.com>
2025-07-31 10:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+window.onload = function () {
+  const button = document.querySelector('input[type="button"]');
+  const select = document.getElementById('colorSelect');
+
+  button.addEventListener('click', function () {
+    const selectedIndex = select.selectedIndex;
+
+    if (selectedIndex !== -1) {
+      select.remove(selectedIndex);
+    }
+  });
+};
